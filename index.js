7772493a6429502f874134a417adc48915747ac3
aleksandr saliy <aleksandr.saliy@bwf.ru>
2022-03-13 19:24:26 +0300

    HomeWork 3.2

--- index.js
+++ index.js
@@ -1,51 +1,31 @@
 #! /usr/bin/env node
 const { Console } = require("console");
-const readline = require("readline");
-const input = readline.createInterface(process.stdin, process.stdout);
 const path = require("path");
 const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers");
 const argv = yargs(hideBin(process.argv)).argv;
 const fs = require("fs");
 
-const quest = function (q) {
-  return new Promise((resolve, reject) => {
-    input.question(q, (ans) => {
-      resolve(ans);
-    });
-  });
-};
-
-async function q() {
-  let makeUpNumber = Math.round(Math.random());
-
-  console.log(`Загадано число в 0 или 1`);
-  result = "0";
-  ans1 = await quest("Введите ваш ответ: ");
-  if (ans1 == makeUpNumber) {
-    console.log("Вы выиграли");
-    result = "1";
-  } else {
-    console.log("Вы проиграли");
-  }
-
-  input.close();
-
+function q() {
   fileName = argv._[0] ? argv._[0].toString() : "default";
   fileName = fileName.includes(".txt") ? fileName : fileName + ".txt";
-  fs.stat(path.join(__dirname, "logs"), (err, stat) => {
-    if (err) {
-      fs.mkdir(path.join(__dirname, "logs"), () => {});
-    }
-  });
 
   fileName = path.join(__dirname, "logs", fileName);
 
   fs.stat(fileName, (err, stats) => {
     if (err) {
-      fs.writeFile(fileName, result, () => {});
+      console.log("Файл не найден");
     } else {
-      fs.appendFile(fileName, "," + result, () => {});
+      fs.readFile(fileName, "utf-8", (err, data) => {
+        if (err) throw new Error(err);
+        data = JSON.parse("[" + data + "]");
+        console.log(`Количество партий: ${data.length}`);
+        win = data.reduce((a, v) => (v > 0 ? a + v : a), 0);
+        winP = Math.round((win / data.length) * 10000) / 100;
+
+        console.log(`Выигранных партий: ${win}, ${winP}%`);
+        console.log(`Проигранных партий: ${data.length - win}, ${100 - winP}%`);
+      });
     }
   });
 }
